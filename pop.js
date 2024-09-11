36006ae9a9ef09d3760a5541340c8d66bf851579
Steven Carlson <mail@stevencarlson.me>
2024-09-11 07:31:55 -0600

    Functionality to remove b tags

--- pop.js
+++ pop.js
@@ -1,5 +1,9 @@
-function popElements(htmlIn) {
-  let htmlOut = htmlIn;
+function popElements(htmlIn, tag) {
+  let tempDiv = document.createElement('div');
+  tempDiv.innerHTML = htmlIn;
+  let selectedTags = tempDiv.querySelectorAll(tag);
+  selectedTags.forEach(tag => {tag.replaceWith(tag.innerHTML);})
+  let htmlOut = tempDiv.innerHTML;
   let outputTextarea = document.getElementById('htmlOutput');
   outputTextarea.value = htmlOut;
 }
